7b826aa9e8537aaa53db94e7626ae3e2f0d1a3b1
Azuzi <azuzii@proton.me>
2025-01-12 14:26:13 +0100

    Added propeties to User

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,4 +1,16 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('user')
-export class User {}
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true, nullable: false })
+  username!: string;
+
+  @Column({ unique: true, nullable: false })
+  email!: string;
+
+  @Column({ nullable: false })
+  password!: string;
+}
