b23225d1cc72dbc8dd5e389046448308ba4f18c1
Azuzi <azuzii@proton.me>
2025-01-13 00:02:52 +0100

    Add validation to RegisterInput and LoginInput DTOs

--- src/modules/auth/dto/auth-input.dto.ts
+++ src/modules/auth/dto/auth-input.dto.ts
@@ -1,12 +1,28 @@
+import { IsAlphanumeric, IsNotEmpty, IsString } from 'class-validator';
+
 export class RegisterInput {
+  @IsNotEmpty()
+  @IsString()
+  @IsAlphanumeric()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
   //   TODO: Add confirm password
 }
 
 export class LoginInput {
-  // TOOD: change to wokr with both email and username
+  // TOOD: change to work with both email and username
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
