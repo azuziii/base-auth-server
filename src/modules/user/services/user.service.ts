2b8b1e95bbc4d44d839cacd92e1626dbe3bc6c12
Azuzi <azuzii@proton.me>
2025-01-12 14:06:28 +0100

    Created user service

---
+++ src/modules/user/services/user.service.ts
@@ -0,0 +1,30 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserInput } from '../dto/user-input.dto';
+import { UserRepository } from '../repositories/user.repository';
+import { User } from '../entities/user.entity';
+import { plainToInstance } from 'class-transformer';
+import { UserOutput } from '../dto/user-output.dto';
+import { IUser } from '../interfaces/user.interface';
+
+@Injectable()
+export class UserService implements IUser {
+  constructor(private readonly repository: UserRepository) {}
+
+  async createUser(user: CreateUserInput): Promise<UserOutput> {
+    const savedUser = await this.repository.save(user);
+
+    return plainToInstance(UserOutput, savedUser, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async doesUsernameExist(username: string): Promise<boolean> {
+    const user = this.repository.getUserByUsername(username);
+    return !!user;
+  }
+
+  async doesEmailExist(email: string): Promise<boolean> {
+    const user = this.repository.getUserByEmail(email);
+    return !!user;
+  }
+}
