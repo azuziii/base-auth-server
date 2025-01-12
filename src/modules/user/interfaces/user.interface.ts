da3dca77755650fca18e85fa978aac45ce075f6e
Azuzi <azuzii@proton.me>
2025-01-12 14:26:22 +0100

    Added getFullUser to IUser

--- src/modules/user/interfaces/user.interface.ts
+++ src/modules/user/interfaces/user.interface.ts
@@ -1,9 +1,12 @@
+import { FindOptionsWhere } from 'typeorm';
 import { CreateUserInput } from '../dto/user-input.dto';
 import { UserOutput } from '../dto/user-output.dto';
+import { User } from '../entities/user.entity';
 
 export const IUser = 'IUser';
 export interface IUser {
   doesEmailExist(email: string): Promise<boolean>;
   doesUsernameExist(username: string): Promise<boolean>;
   createUser(user: CreateUserInput): Promise<UserOutput>;
+  getFullUser(options?: FindOptionsWhere<User>): Promise<User>;
 }
