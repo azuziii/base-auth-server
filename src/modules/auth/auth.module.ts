3a6b6bbab97e69788f524bce9363ffbaf0c924aa
Azuzi <azuzii@proton.me>
2025-01-13 00:03:15 +0100

    Add SessionModule to AuthModule for session management

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserModule } from '../user/user.module';
+import { SessionModule } from '../session/session.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    // Note: SessionModule used with SessionInterceptor, and in logout method
+    SessionModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
